ef1bac8f4fc0efda1a94776c048b6b1e2f77e13f
MOHAMMED SUHAIB S <mohammedsuhaib.0294@gmail.com>
2024-08-11 15:54:14 +0530

    hero-section

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,36 @@
 // src/App.js
-import React from 'react';
+import React, { useState } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './Components/home'
+import Home from './Components/home/home'
 import Diabetes from './Components/Diabetes'; 
-import Navbar from './Components/Navbar'; 
+import Navbar from './Components/Navbar/Navbar'; 
 import About from './Components/About';
-import './Navbar.css';
+import './Components/Navbar/Navbar.css';
 import './App.css';
+import Background from './Components/Background/Background';
 
 function App() {
+
+    let heroData = [
+        {text1:'Dive into', text2: 'what you love'},
+        {text1:'Indulge', text2: 'your passion'},
+        {text1:'Give in to ', text2: 'your passion'},
+    ]
+    const[heroCount, setHeroCount] = useState(0);
+    const[playStatus, setPlayStatus] = useState(false);
+
+
+
     const router = createBrowserRouter([
         {
             path: "/",
-            element: <><Navbar/><Home/></>
+            element: <><Navbar/><Home
+            setPlayStatus={setPlayStatus}
+            heroData={heroData[heroCount]}
+            heroCount={heroCount}
+            setHeroCount={setHeroCount}
+            playStatus={playStatus}
+            /></>
         },
         {
             path : "/Dia",
@@ -27,6 +45,7 @@ function App() {
     return (
         <>
        {/* <Navbar/> */}
+       <Background playStatus={playStatus} heroCount={heroCount}/>
        <RouterProvider router={router}/>
        {/* <Home/> */}
        {/* <Diabetes/> */}
