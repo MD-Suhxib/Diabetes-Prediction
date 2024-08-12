c7065e7b56770987c0db5262d48c09bee4cc7bc4
MOHAMMED SUHAIB S <mohammedsuhaib.0294@gmail.com>
2024-08-12 20:14:20 +0530

    app wrapup

--- client/src/Components/home/home.jsx
+++ client/src/Components/home/home.jsx
@@ -1,7 +1,10 @@
+import React from 'react' ;
 import './home.css'
-import {Link} from 'react-router-dom'
+// import {Link} from 'react-router-dom'
 import arrow_btn from '../../Assets/arrow_btn.png'
-const home = ({heroData,setHeroCount,heroCount,setPlayStatus,playStatus}) => {
+import { useNavigate } from 'react-router-dom'
+const Home = ({heroData,setHeroCount,heroCount,setPlayStatus,playStatus}) => {
+  const navigate = useNavigate();
   return (
     <div className='hero'>
       <div className="hero-text">
@@ -13,20 +16,21 @@ const home = ({heroData,setHeroCount,heroCount,setPlayStatus,playStatus}) => {
       </div>
       <div className="hero-about" >
         <p>Download Sample Data</p>
-        <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer">
+        <a href="https://docs.google.com/document/d/1TeLZ-AC535_Vdl32lnT9d-AuuOF4OF0g/edit?usp=
+        sharing&ouid=116673282843835837563&rtpof=true&sd=true" target="_blank" rel="noopener noreferrer">
         <img src={arrow_btn} alt="" />
         </a>
+        <button onClick={()=> navigate ("/features")} className="sample-button">Predict</button>
         
+
       </div>
       <div className="hero-play">
         <ul className="hero-dots">
-          {/* <li onClick={()=>setHeroCount(0)}  className={heroCount===0?'hero-dot orange':"hero-dot"}></li>
-          <li onClick={()=>setHeroCount(1)}  className={heroCount===1?'hero-dot orange':"hero-dot"}></li>
-          <li onClick={()=>setHeroCount(2)}  className={heroCount===2?'hero-dot orange':"hero-dot"}></li> */}
+          
         </ul>
       </div>
     </div>
   )
 }
 
-export default home
+export default Home
